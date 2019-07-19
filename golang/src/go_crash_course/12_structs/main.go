9c2ce1d2dc3344be1a04ce1ca10f4f98f771279a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-07-19 09:01:53 -0400

    structs - value receiver method

--- golang/src/go_crash_course/12_structs/main.go
+++ golang/src/go_crash_course/12_structs/main.go
@@ -1,6 +1,7 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "strconv")
 
 // Define person struct
 type Person struct {
@@ -11,13 +12,22 @@ type Person struct {
   age int
 }
 
+// Greeting method (value receiver)
+// have to convert int age to string for it this to work
+func (p Person) greet() string {
+  return "Hello, my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age)
+}
+
 func main() {
   // initialize person using struct
   person1 := Person{firstName: "Emily", lastName: "Pleiad", city: "New Meeting", gender: "f", age: 18}
 
   // alternative
-  person1 := Person{"Emily","Pleiad", "New Meeting", "f", 18}
+  // person1 := Person{"Emily","Pleiad", "New Meeting", "f", 18}
 
-  fmt.Println(person1)
   fmt.Println(person1.firstName)
+  person1.age++
+  fmt.Println(person1)
+
+  fmt.Println(person1.greet())
 }
