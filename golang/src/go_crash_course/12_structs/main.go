08019edbf99ec92e3815be1f14a97acc77b82757
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-07-20 09:12:41 -0400

    structs - pointer receiver method

--- golang/src/go_crash_course/12_structs/main.go
+++ golang/src/go_crash_course/12_structs/main.go
@@ -5,11 +5,14 @@ import ("fmt"
 
 // Define person struct
 type Person struct {
-  firstName string
-  lastName string
-  city string
-  gender string
-  age int
+  // firstName string
+  // lastName string
+  // city string
+  // gender string
+  // age int
+
+  firstName, lastName, city, gender string
+  age                               int
 }
 
 // Greeting method (value receiver)
@@ -18,16 +21,36 @@ func (p Person) greet() string {
   return "Hello, my name is " + p.firstName + " " + p.lastName + " and I am " + strconv.Itoa(p.age)
 }
 
+// hasBirthday method (pointer receiver)
+func (p *Person) hasBirthday() {
+  p.age++
+}
+
+// getMarried (pointer receiver)
+func (p *Person) getMarried(spouseLastName string) {
+  if p.gender == "m" {
+    return
+  } else {
+    p.lastName = spouseLastName
+  }
+}
+
 func main() {
   // initialize person using struct
   person1 := Person{firstName: "Emily", lastName: "Pleiad", city: "New Meeting", gender: "f", age: 18}
 
   // alternative
-  // person1 := Person{"Emily","Pleiad", "New Meeting", "f", 18}
+  person2 := Person{"Adam","Jura", "Rogerstowne", "m", 18}
 
   fmt.Println(person1.firstName)
   person1.age++
   fmt.Println(person1)
+
+  person1.hasBirthday()
+  person1.hasBirthday()
+  person1.getMarried("Jura")
+
+  person2.getMarried("Pleiad")
 
   fmt.Println(person1.greet())
 }
